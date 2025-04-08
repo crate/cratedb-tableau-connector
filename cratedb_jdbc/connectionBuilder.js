66df0c7aabd99b3624c1232cb10248f46f8bdcfb
hlcianfagna <110453267+hlcianfagna@users.noreply.github.com>
2025-04-08 17:43:53 +0100

    Test

--- cratedb_jdbc/connectionBuilder.js
+++ cratedb_jdbc/connectionBuilder.js
@@ -1,5 +1,5 @@
 (function dsbuilder(attr) {
-    var urlBuilder = "jdbc:postgresql://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + "?";
+    var urlBuilder = "jdbc:postgresql://" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + "/" + attr[connectionHelper.attributeDatabase] + "?useCursorFetch=true&defaultRowFetchSize=100&autoCommit=false";
 
     return [urlBuilder];
 })
